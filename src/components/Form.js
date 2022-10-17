7da6b7f10348825dc40169c9d72703df69b0cdcd
The-SaqlainNawaz <pacsaqiirr@gmail.com>
2022-10-17 21:54:29 +0500

    Just a moment away

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,50 @@
 import React from "react";
 import "./style.css";
 import MemeImage from "./Meme-image";
-import memedata from "../memedata";
 
 let img_url;
 export default function Form() {
     /**
-     * Challenge: Update our state to save the meme-related
-     * data as an object called `meme`. It should have the
-     * following 3 properties:
-     * topText, bottomText, randomImage.
-     *
-     * The 2 text states can default to empty strings for now,
-     * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
-     *
-     * Next, create a new state variable called `allMemeImages`
-     * which will default to `memesData`, which we imported above
-     *
-     * Lastly, update the `getMemeImage` function and the markup
-     * to reflect our newly reformed state object and array in the
-     * correct way.
+        * Challenge: 
+        * As soon as the Meme component loads the first time,
+        * make an API call to "https://api.imgflip.com/get_memes".
+        * 
+        * When the data comes in, save just the memes array part
+        * of that data to the `allMemes` state
+        * 
+        * Think about if there are any dependencies that, if they
+        * changed, you'd want to cause to re-run this function.
+        * 
+        * Hint: for now, don't try to use an async/await function.
+        * Instead, use `.then()` blocks to resolve the promises
+        * from using `fetch`. We'll learn why after this challenge.
      */
+
+    const [allMemeImages, setAllMemeImages] = React.useState("");
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemeImages(data))
+    }, [])
+    console.log(JSON.stringify(allMemeImages))
+
+    console.log(allMemeImages.data.memes[1].url)
+
+    const [memeText, setMemeText] = React.useState({
+        topText: "",
+        bottomText: ""
+    })
+
+    function onHandler(event) {
+        const { name, value } = event.target
+        setMemeText(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
 
     //   meme state
     const [meme, setMeme] = React.useState({
@@ -30,15 +54,11 @@ export default function Form() {
     });
 
     //   all memes state
-    const [allMemeImages, setAllMemeImages] = React.useState(memedata);
 
     function getMemeImage() {
-
         const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        console.log(randomNumber)
         img_url = memesArray[randomNumber].url
-        console.log(img_url)
         setMeme((prev) => ({
             ...prev,
             randomImage: img_url
@@ -53,18 +73,24 @@ export default function Form() {
                         type="text"
                         placeholder="Top Text"
                         className="form--input"
+                        name="topText"
+                        value={memeText.topText}
+                        onChange={onHandler}
                     ></input>
                     <input
                         type="text"
                         placeholder="bottom Text"
                         className="form--input"
+                        name="bottomText"
+                        value={memeText.bottomText}
+                        onChange={onHandler}
                     ></input>
                     <button className="form--button" onClick={getMemeImage}>
                         Generator New Meme Image
                     </button>
                 </div>
             </div>
-            <MemeImage img={meme.randomImage} />
+            <MemeImage img={meme.randomImage} imgText={memeText} />
         </>
     );
 }
--- src/components/Meme-image.js
+++ src/components/Meme-image.js
@@ -4,8 +4,9 @@ import "./style.css";
 export default function Meme_Image(props) {
     return (
         <div className="meme--image">
-            {/* <p>{props.img}</p> */}
+            <h2 className="meme--text top">{props.imgText.topText}</h2>
             <img src={props.img} alt="" className="img--meme"></img>
+            <h2 className="meme--text bottom">{props.imgText.bottomText}</h2>
         </div>
     );
 }
